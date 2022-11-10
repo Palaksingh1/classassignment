c5c8a919cef52b9d7b73807391384a4dd75692d1
Palaksingh1 <117344679+Palaksingh1@users.noreply.github.com>
2022-11-10 17:05:39 +0530

    Add files via upload

---
+++ LoopStarPattern2.java
@@ -0,0 +1,19 @@
+package basicprogram;
+
+public class LoopStarPattern2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        for(int i=5;i>=1;i--)
+        {
+        	for(int j=1;j<=i;j++)
+        	{
+        		System.out.print("*");
+        	}
+        	
+        	System.out.println();
+        }
+        
+	}
+
+}
